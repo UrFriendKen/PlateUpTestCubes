bd7bfc69c8bc5976e88e23ba1b267657dec7ff11
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-07-18 20:10:20 +0800

    Refactored to use string ID

--- Main.cs
+++ Main.cs
@@ -1,45 +1,35 @@
-﻿using KitchenLib;
-using KitchenMods;
+﻿using KitchenMods;
 using System.Linq;
-using System.Reflection;
 using UnityEngine;
 
 // Namespace should have "Kitchen" in the beginning
 namespace TestCubes
 {
-    public class Main : BaseMod, IModSystem
+    public class Main : IModInitializer
     {
-        // GUID must be unique and is recommended to be in reverse domain name notation
-        // Mod Name is displayed to the player and listed in the mods menu
-        // Mod Version must follow semver notation e.g. "1.2.3"
         public const string MOD_GUID = "IcedMilo.PlateUp.TestCubes";
         public const string MOD_NAME = "Test Cubes";
-        public const string MOD_VERSION = "0.1.0";
-        public const string MOD_AUTHOR = "IcedMilo";
-        public const string MOD_GAMEVERSION = ">=1.1.5";
-        // Game version this mod is designed for in semver
-        // e.g. ">=1.1.3" current and all future
-        // e.g. ">=1.1.3 <=1.2.3" for all from/until
+        public const string MOD_VERSION = "0.1.1";
 
         public static AssetBundle Bundle;
 
-        public Main() : base(MOD_GUID, MOD_NAME, MOD_AUTHOR, MOD_VERSION, MOD_GAMEVERSION, Assembly.GetExecutingAssembly()) { }
+        public Main()
+        {
+        }
 
-        protected override void OnInitialise()
+        public void PostActivate(KitchenMods.Mod mod)
         {
             LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!");
-        }
 
-        protected override void OnUpdate()
-        {
-        }
-
-        protected override void OnPostActivate(KitchenMods.Mod mod)
-        {
             LogInfo("Attempting to load asset bundle...");
             Bundle = mod.GetPacks<AssetBundleModPack>().SelectMany(e => e.AssetBundles).First();
             LogInfo("Done loading asset bundle.");
         }
+
+        public void PreInject() { }
+
+        public void PostInject() { }
+
         #region Logging
         public static void LogInfo(string _log) { Debug.Log($"[{MOD_NAME}] " + _log); }
         public static void LogWarning(string _log) { Debug.LogWarning($"[{MOD_NAME}] " + _log); }
--- TestCubeManager.cs
+++ TestCubeManager.cs
@@ -1,6 +1,4 @@
-﻿using KitchenLib.Customs;
-using KitchenLib.Utils;
-using System;
+﻿using KitchenLib.Utils;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,7 +7,7 @@ namespace TestCubes
     public static class TestCubeManager
     {
         private static GameObject _testCubesHider;
-        private static Dictionary<Type, GameObject> _testCubePrefabs;
+        private static Dictionary<string, GameObject> _testCubePrefabs;
 
         /// <summary>
         /// Gets the cube prefab for a specified Type key. If prefab does not exist, creates a new cube prefab.
@@ -23,10 +21,29 @@ namespace TestCubes
         /// <returns>Prefab GameObject for instantiation</returns>
         public static GameObject GetPrefab<T>(float scaleX = 1f, float scaleY = 1f, float scaleZ = 1f, Material material = null, bool collider = true)
         {
-            Type type = typeof(T);
+            return GetPrefab(typeof(T).AssemblyQualifiedName,
+                scaleX: scaleX,
+                scaleY: scaleY,
+                scaleZ: scaleZ,
+                material: material,
+                collider: collider);
+        }
+
+        /// <summary>
+        /// Gets the cube prefab for a specified Identifier. If prefab does not exist, creates a new cube prefab.
+        /// </summary>
+        /// <param name="guid">Unique Object Identifier</param>
+        /// <param name="scaleX">X component of local scale</param>
+        /// <param name="scaleY">Y component of local scale</param>
+        /// <param name="scaleZ">Z component of local scale</param>
+        /// <param name="material">Cube material</param>
+        /// <param name="collider">If true, collider and navmesh enabled. Otherwise, disabled.</param>
+        /// <returns>Prefab GameObject for instantiation</returns>
+        public static GameObject GetPrefab(string guid, float scaleX = 1f, float scaleY = 1f, float scaleZ = 1f, Material material = null, bool collider = true)
+        {
             if (_testCubePrefabs == null)
             {
-                _testCubePrefabs = new Dictionary<Type, GameObject>();
+                _testCubePrefabs = new Dictionary<string, GameObject>();
             }
 
             if (_testCubesHider == null)
@@ -35,10 +52,10 @@ namespace TestCubes
                 _testCubesHider.SetActive(false);
             }
 
-            if (!_testCubePrefabs.TryGetValue(type, out GameObject prefab))
+            if (!_testCubePrefabs.TryGetValue(guid, out GameObject prefab))
             {
                 prefab = GameObject.Instantiate(Main.Bundle.LoadAsset<GameObject>("TestCube"));
-                prefab.name = $"{type.Name}_TestCube";
+                prefab.name = $"{guid}_TestCube";
                 prefab.transform.SetParent(_testCubesHider.transform);
                 Transform anchor = prefab.transform.Find("Anchor");
                 if (anchor != null)
@@ -54,7 +71,7 @@ namespace TestCubes
                     MaterialUtils.ApplyMaterial(prefab, "Anchor/Cube", new Material[] { material });
                 }
 
-                _testCubePrefabs[type] = prefab;
+                _testCubePrefabs[guid] = prefab;
             }
 
             return prefab;
